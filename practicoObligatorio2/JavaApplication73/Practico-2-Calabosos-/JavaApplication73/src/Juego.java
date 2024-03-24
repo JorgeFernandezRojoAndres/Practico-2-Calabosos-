ad945823816c12fcf09ba49e3d43c846517d5238
Cintia <147344803+CintiaBarroso@users.noreply.github.com>
2024-03-23 23:59:25 -0300

    optimizacion del main del juego
    Este código ejecuta una simulación de la batalla entre Superman y Lex Luthor, donde cada uno ataca al otro por turnos hasta que uno de ellos queda derrotado. Luego, imprime un mensaje indicando el resultado de la batalla.

--- practicoObligatorio2/JavaApplication73/Practico-2-Calabosos-/JavaApplication73/src/Juego.java
+++ practicoObligatorio2/JavaApplication73/Practico-2-Calabosos-/JavaApplication73/src/Juego.java
@@ -4,39 +4,21 @@ import java.util.Scanner;
 // Clase principal del juego
 public class Juego {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    SuperHeroe superman = new SuperHeroe("Superman", 10, 100);
+    Villano lexLuthor = new Villano("Lex Luthor", 8, 80);
 
-        // Crear dos superhéroes
-        SuperHeroe marvelHeroe = new SuperHeroe("Iron Man", 20, 100);
-        SuperHeroe capcomHeroe = new SuperHeroe("Ryu", 25, 100);
-
-        System.out.println("¡Bienvenido a la batalla de Superhéroes!");
-
-        // Loop principal del juego
-        while (marvelHeroe.estaVivo() && capcomHeroe.estaVivo()) {
-            System.out.println("\nTurno de Marvel: " + marvelHeroe.getNombre());
-            System.out.println("Atacar a Capcom con cuánta fuerza? (0-" + marvelHeroe.getFuerza() + ")");
-            int ataqueMarvel = scanner.nextInt();
-
-            System.out.println("Turno de Capcom: " + capcomHeroe.getNombre());
-            System.out.println("Atacar a Marvel con cuánta fuerza? (0-" + capcomHeroe.getFuerza() + ")");
-            int ataqueCapcom = scanner.nextInt();
-
-            // Aplicar daño
-            capcomHeroe.recibirDanio(ataqueMarvel);
-            marvelHeroe.recibirDanio(ataqueCapcom);
-
-            // Mostrar estado de la batalla
-            System.out.println("\nEstado de la batalla:");
-            System.out.println("Marvel: Vida = " + marvelHeroe.getVida());
-            System.out.println("Capcom: Vida = " + capcomHeroe.getVida());
-        }
-
-        // Determinar al ganador
-        if (marvelHeroe.estaVivo()) {
-            System.out.println("\n¡Marvel ha ganado la batalla!");
-        } else {
-            System.out.println("\n¡Capcom ha ganado la batalla!");
+    while (superman.estaVivo() && lexLuthor.estaVivo()) {
+        superman.atacarVillano(lexLuthor);
+        if (lexLuthor.estaVivo()) {
+            lexLuthor.atacarSuperHeroe(superman);
         }
     }
+
+    if (superman.estaVivo()) {
+        System.out.println("¡Superman ha derrotado a Lex Luthor y salvado el día!");
+    } else {
+        System.out.println("¡Lex Luthor ha derrotado a Superman y se ha apoderado del mundo!");
+    }
 }
+    }
+
